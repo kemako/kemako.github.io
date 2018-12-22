39944778323b322a7b0c0893936cef44d6fbd619
kemako <miyatake-mako21f@g.ecc.u-tokyo.ac.jp>
2018-12-22 18:16:38 +0900

    Add animation to all page

--- js/script.js
+++ js/script.js
@@ -53,37 +53,97 @@ $(function() {
       $nav.removeClass('fixed');
       $("#contents").css("margin-top", "0");
     }
-  });
+  })
+
+  // Wrap every letter in a span
+$('.ml1 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
 });
-
-var scrollAnimationClass = 'sa';
-var scrollAnimationShowClass = 'show';
-var triggerMarginDefault = 300;
-
-var scrollAnimationElm = document.querySelectorAll('.' + scrollAnimationClass);
-var scrollAnimationFunc = function() {
-  var dataMargin = scrollAnimationClass + '_margin';
-  var dataTrigger = scrollAnimationClass + '_trigger';
-  var dataDelay = scrollAnimationClass + '_delay';
-  for(var i = 0; i < scrollAnimationElm.length; i++) {
-    var triggerMargin = triggerMarginDefault;
-    var elm = scrollAnimationElm[i];
-    var showPos = 0;
-    if(elm.dataset[dataMargin] != null) {
-      triggerMargin = parseInt(elm.dataset[dataMargin]);
+$('.ml6 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+$('.ml16').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: function(el, i) {
+      return 70 * (i+1)
     }
-    if(elm.dataset[dataTrigger]) {
-      showPos = document.querySelector(elm.dataset[dataTrigger]).getBoundingClientRect().top + triggerMargin;
-    } else {
-      showPos = elm.getBoundingClientRect().top + triggerMargin;
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: function(el, i, l) {
+      return 80 * (l - i);
     }
-    if (window.innerHeight > showPos) {
-      var delay = (elm.dataset[dataDelay])? elm.dataset[dataDelay] : 0;
-      setTimeout(function(index) {
-        scrollAnimationElm[index].classList.add('show');
-      }.bind(null, i), delay);
+  });
+
+  // Wrap every letter in a span
+
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: function(el, i) {
+      return 50 * i;
+    }
+  });
+
+  // Wrap every letter in a span
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml16 .letter',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: function(el, i) {
+      return 30 * i;
+    }
+  });
+
+  var scrollAnimationClass = 'sa';
+  var scrollAnimationShowClass = 'show';
+  var triggerMarginDefault = 300;
+
+  var scrollAnimationElm = document.querySelectorAll('.' + scrollAnimationClass);
+  var scrollAnimationFunc = function() {
+    var dataMargin = scrollAnimationClass + '_margin';
+    var dataTrigger = scrollAnimationClass + '_trigger';
+    var dataDelay = scrollAnimationClass + '_delay';
+    for(var i = 0; i < scrollAnimationElm.length; i++) {
+      var triggerMargin = triggerMarginDefault;
+      var elm = scrollAnimationElm[i];
+      var showPos = 0;
+      if(elm.dataset[dataMargin] != null) {
+        triggerMargin = parseInt(elm.dataset[dataMargin]);
+      }
+      if(elm.dataset[dataTrigger]) {
+        showPos = document.querySelector(elm.dataset[dataTrigger]).getBoundingClientRect().top + triggerMargin;
+      } else {
+        showPos = elm.getBoundingClientRect().top + triggerMargin;
+      }
+      if (window.innerHeight > showPos) {
+        var delay = (elm.dataset[dataDelay])? elm.dataset[dataDelay] : 0;
+        setTimeout(function(index) {
+          scrollAnimationElm[index].classList.add('show');
+        }.bind(null, i), delay);
+      }
     }
   }
-}
-window.addEventListener('load', scrollAnimationFunc);
-window.addEventListener('scroll', scrollAnimationFunc);
+  window.addEventListener('load', scrollAnimationFunc);
+  window.addEventListener('scroll', scrollAnimationFunc);
+});
